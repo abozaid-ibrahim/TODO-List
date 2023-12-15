6bc351c3125d56b9f2a1c65e31236a526601f57b
Abozaid Ibrahim <abozaid.ibrahim11@gmail.com>
2023-12-15 15:50:46 +0200

    Create Xcode Project

---
+++ TODO List/TODO_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODO_ListApp.swift
+//  TODO List
+//
+//  Created by abuzeid on 15.12.23.
+//
+
+import SwiftUI
+
+@main
+struct TODO_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
